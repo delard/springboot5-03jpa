db1ee071c32b7f739304777b4c2d543972521a0d
delard <david.delarosadelgado@gmail.com>
2019-04-14 01:58:35 +0200

    53 - Creando la vista ver para visualizar la imagen del cliente

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,6 +37,29 @@ public class ClienteController {
 	@Autowired
 	private IClienteService clienteService;
 
+	@GetMapping(value="/ver/{id}")
+	String ver(@PathVariable(value="id") Long id, Map<String,Object> model, RedirectAttributes flash) {
+		
+		Cliente cliente;
+		
+		if (id>0) {
+			cliente = clienteService.findOne(id);
+			if (cliente == null) {
+				flash.addFlashAttribute("error", "El ID del cliente no existe en la BBDD");
+				return "redirect:/listar";				
+			}
+		} else {
+			flash.addFlashAttribute("error", "El ID del cliente no puede ser cero");
+			return "redirect:/listar";
+		}
+		
+		model.put("cliente", cliente);
+		model.put("titulo", "Detalle de cliente: " + cliente.getNombre() 
+				+ " " + cliente.getApellido());
+		return "ver";
+	}
+	
+	
 	@RequestMapping(value = "/listar", method = RequestMethod.GET)
 	public String listar(@RequestParam(name="page", defaultValue="0") int page, Model model) {
 		
